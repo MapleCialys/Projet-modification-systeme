520507bee55f46823f35a4df4ad89e529c1b899e
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-08-21 21:48:17 -0400

    Bouton en page d'accueil

--- devflix/src/routes/+page.svelte
+++ devflix/src/routes/+page.svelte
@@ -1,16 +1,13 @@
-<h1>Sign in</h1>
+<script>
+    function goLogin()
+    {
+        window.location.href='/login';
+    }
 
-<form method="POST" action="?/signin">
-
-    <label for="nom">Nom</label>
-    <input type="text" name="nom" id="nom">
-
-    <label for="prenom">Prenom</label>
-    <input type="text" name="prenom" id="prenom">
-
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
-
-    <input type="submit" value="signin">
-
-</form>
+    function goSignin()
+    {
+        window.location.href='/users/new';
+    }
+</script>
+<button on:click={goLogin}>Se connecter</button>
+<button on:click={goSignin}>Creer un compte</button>
--- devflix/src/routes/login/+page.server.js
+++ devflix/src/routes/login/+page.server.js
@@ -1,4 +1,5 @@
 import { authenticate } from "../../lib/db/controllers/users.controller";
+import { createCookie } from "../../lib/db/controllers/sessions.controller";
 
 export const actions = {
 
@@ -7,7 +8,7 @@ export const actions = {
         console.log("Data : ", data)
         try{
             const auth = await authenticate(data.get("nom"), data.get("prenom"), data.get("password"));
-
+            createCookie(auth.id, cookies);
             console.log("Connexion réussie : ", auth);
         }catch(error){
             console.log("Erreur lors de la connexion : ", error);
