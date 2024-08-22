188bac41e608a3495dd9957d8973d992b703e12f
Aurore Langlet <aurore_langlet@etik.com>
2024-08-22 19:19:47 -0400

    Annuler le style de "routes/+layout.svelte

---
+++ devflix/src/routes/items/+layout.svelte
@@ -0,0 +1,11 @@
+<style>
+    /* Annuler le style de "routes/+layout.svelte" */
+    :global(body) {
+        color: initial !important;
+        background-color: initial !important;
+        transform: initial !important;
+        cursor: initial !important;
+    }
+</style>
+
+<slot/>
