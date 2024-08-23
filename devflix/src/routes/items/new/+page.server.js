190d90e8aaf70bd4dcf7994085e03bf79dee8775
Aurore Langlet <aurore_langlet@etik.com>
2024-08-22 20:22:26 -0400

    Ajout data manquants

--- devflix/src/routes/items/new/+page.server.js
+++ devflix/src/routes/items/new/+page.server.js
@@ -6,7 +6,7 @@ export const actions = {
     new: async({ cookies, request })=>{
         const data = await request.formData();
 
-        let res = await newItem(data.get("nom"), data.get("description"), data.get("quantite"));
+        let res = await newItem(data.get("nom"), data.get("description"), data.get("image_item"), data.get("bande_annonce"), data.get("date"), data.get("quantite_disponible"));
 
         console.log(res);
     }
