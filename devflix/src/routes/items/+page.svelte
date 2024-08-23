ebf7dc082ed6429235e5b6099997e1c245a20e21
Aurore Langlet <aurore_langlet@etik.com>
2024-08-22 21:57:09 -0400

    Modif page items

--- devflix/src/routes/items/+page.svelte
+++ devflix/src/routes/items/+page.svelte
@@ -6,19 +6,24 @@
 
     console.log(items)
 
+    import Title from '../../lib/components/title.svelte';
+
+    function formatDate(dateString) {
+        const options = { day: '2-digit', month: 'long', year: 'numeric' };
+        return new Date(dateString).toLocaleDateString('fr-FR', options);
+    }
+
 </script>
 
 <div class="block">
-    <div class="block">
-        <h1 class="title">Catalogue de films</h1>
-    </div>
+    <Title title={"Catalogue de films"}></Title>
 
     <div class="grid is-col-min-9">
-        {#each items as item}
         <div class="cell">
+            {#each items as item}
             <div class="card">
                 <div class="card-image">
-                    <figure class="image is-5by3">
+                    <figure class="image image image is-2by3">
                         <a href="./items/{item.id}">
                             <img src="{item.image_item}" alt="{item.nom}"/>
                         </a>
@@ -27,13 +32,14 @@
                 <div class="card-content">
                     <div class="content">
                         <h2 class="title is-4">{item.nom}</h2>
-                        <p class="subtitle is-6">{item.date}</p>
-                        <p>{item.quantite_disponible}</p>
-                        <a href="/" class="button is-primary">Ajouter au panier</a>
+                        <p class="subtitle is-6">{formatDate(item.date)}</p>
+                        <p>Quantité disponible : {item.quantite_disponible}</p>
+                        <a href="./panier" class="button is-primary">Ajouter au panier</a>
                     </div>
                 </div>
             </div>
+            {/each}
         </div>
-        {/each}
     </div>
+
 </div>
