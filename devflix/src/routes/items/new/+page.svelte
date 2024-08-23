46fe53336ea4ac063a597395bfe1cfb96490736c
Aurore Langlet <aurore_langlet@etik.com>
2024-08-22 20:26:17 -0400

    Ajout champs formulaire

--- devflix/src/routes/items/new/+page.svelte
+++ devflix/src/routes/items/new/+page.svelte
@@ -1,14 +1,23 @@
-<h1>New Item</h1>
+<h1 class="title">Ajouter un film</h1>
 
 <form method="POST" action="?/new">
     <label for="nom">Nom</label>
-    <input type="text" name="nom" id="nom">
+    <input type="text" name="nom" id="nom" required>
 
     <label for="description">Description</label>
-    <input type="text" name="description" id="description">
+    <input type="text" name="description" id="description" required>
 
-    <label for="quantite">Quantite</label>
-    <input type="number" name="quantite" id="quantite">
+    <label for="image_item"></label>
+    <input type="text" name="image_item" id="image_item">
+
+    <label for="bande_annonce"></label>
+    <input type="url" name="bande_annonce" id="bande_annonce">
+
+    <label for="date"></label>
+    <input type="date" name="date" id="date">
+
+    <label for="quantite_disponible">Quantité disponible</label>
+    <input type="number" name="quantite_disponible" id="quantite_disponible" required>
 
     <input type="submit" value="Envoyer">
 </form>
