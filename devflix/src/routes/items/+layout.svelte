1c4df9dbabd4bbaf49a298d429121a30e411bf71
Aurore Langlet <aurore_langlet@etik.com>
2024-08-26 18:01:58 -0400

    Améliorations catalogue

--- devflix/src/routes/items/+layout.svelte
+++ devflix/src/routes/items/+layout.svelte
@@ -3,16 +3,11 @@
 </script>
 
 <style>
-    /* Container et redimmension automatique pour les images d'affiches de films */
+    /* Container pour les images d'affiches de films */
     :global(.image-container) {
           display: flex;
           justify-content: center;
       }
-    :global(.resized-image) {
-        width: auto;
-        max-width: 560px;
-        height: 380px;
-    }
 </style>
 
 <slot/>
--- devflix/src/routes/items/+page.svelte
+++ devflix/src/routes/items/+page.svelte
@@ -17,35 +17,42 @@
 <div class="block">
     <H1Title title={"Catalogue de films"}></H1Title>
 
-    <div class="block">
+    <div class="block has-text-right">
         <ButtonPrimary url={"/items/new"} texte={"Ajouter un film"}></ButtonPrimary>
     </div>
     
-    
-    <!-- Grille avec cards pour chaque film -->
-    <div class="grid is-col-min-9">
-        <div class="cell">
-            {#each items as item}
-            <div class="card">
-                <div class="card-image">
-                    <figure>
-                        <a href="./items/{item.id}">
-                            <img src="{item.image_item}" alt="{item.nom}" class="resized-image"/>
-                        </a>
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="content">
-                        <h2 class="title is-4">{item.nom}</h2>
-                        <p class="subtitle is-6">{formatDate(item.date)}</p>
-                        <p>Quantité disponible : {item.quantite_disponible}</p>
 
-                        <ButtonPrimary url={"./panier"} texte={"Ajouter au panier"}></ButtonPrimary>
+    <!-- Grille avec cards pour chaque film -->
+    <div class="grid is-col-min-10">
+        {#each items as item}
+            <div class="cell is-flex">
+                <div class="card">
+
+                    <div class="card-image"> <!-- Image -->
+                        <figure>
+                            <a href="./items/{item.id}">
+                                {#if item.image_item} <!-- Image générique si image_item est NULL -->
+                                <img src="{item.image_item}" alt="{item.nom}"/>
+                                {:else}
+                                <img src="/affiche_default.jpg" alt="{item.nom}"/>
+                                {/if}
+                            </a>
+                        </figure>
                     </div>
+                    
+
+                    <div class="card-content"> <!-- Contenu -->
+                        <div class="content">
+                            <h2 class="title is-4 is-thin">{item.nom}</h2>
+                            <p class="subtitle is-6">{formatDate(item.date)}</p>
+                            <p>Quantité disponible : {item.quantite_disponible}</p>
+                            <ButtonPrimary url={"./panier"} texte={"Ajouter au panier"}></ButtonPrimary>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-            {/each}
-        </div>
+        {/each}
     </div>
 
 </div>
