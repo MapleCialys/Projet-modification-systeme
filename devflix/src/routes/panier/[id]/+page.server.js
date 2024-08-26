aa75d95d77e70c53cd76f9cac47c0a469a55d18b
Aurore Langlet <aurore_langlet@etik.com>
2024-08-26 17:14:39 -0400

    J'en ai ma claque...

--- devflix/src/routes/panier/[id]/+page.server.js
+++ devflix/src/routes/panier/[id]/+page.server.js
@@ -1,3 +1,12 @@
+/* Permet d'afficher les infos des films dans le panier */
+import { findAll } from "../../lib/db/controllers/items.controller";
+
+export async function load({params}) {
+    const items = await findAll();
+    return { items:items };
+}
+
+/* Panier */
 import { findOne } from "$lib/db/controllers/panier.controller.js";
 
 export const actions = {
--- devflix/src/routes/panier/[id]/+page.svelte
+++ devflix/src/routes/panier/[id]/+page.svelte
@@ -1,7 +1,8 @@
 <script>
 
     export let data;
-
+    
+    const items = data.items;
     const panier = data.panier;
 
     console.log(panier)
@@ -13,5 +14,37 @@
 <div class="block">
     <H1Title title={"Panier"}></H1Title>
 
-    
+    <div>
+        <button class="button is-danger">Vider le panier</button>
+    </div>
+
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th></th> <!-- Cases à cocher -->
+                    <th></th> <!-- Miniatures affiches de films -->
+                    <th>Films</th>
+                    <th></th> <!-- Boutons supprimer (C12) -->
+                </tr>
+            </thead>
+            <tbody>
+                {#each items as item}
+                <tr>
+                    <td>
+                        <input type="checkbox">
+                        <input type="number" name="id" id="id" value={item.id} readonly hidden>
+                    </td>
+                    <td><figure class="image-container"><img src="{item.image_item}" alt="{item.nom}" class="miniature-image"/></figure></td>
+                    <td><p>Description : {item.description}</p></td>
+                    <td>
+                        <i class="fa-solid fa-trash"></i>
+                        <span class="icon"><i class="fa-solid fa-trash"></i></span>
+                    </td>
+                </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+
 </div>
