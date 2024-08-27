0e7b94bb37d22c161c8dfb2a81d4e7b840bdc997
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-08-27 00:03:57 -0400

    Ajout CSS page d'accueil

--- devflix/src/routes/+page.svelte
+++ devflix/src/routes/+page.svelte
@@ -8,10 +8,36 @@
     {
         window.location.href='/users/new';
     }
+    import logo from "$lib/img/app/logo.png";
 </script>
 <script context="module">
     export let cookie;
     console.log(cookie);
 </script>
-<button on:click={goLogin}>Se connecter</button>
-<button on:click={goSignin}>Creer un compte</button>
+<section class="hero is-fullheight header-image">
+    <div class="hero-head is-right m-4">
+        <div class="buttons is-right">
+        <button class="button is-warning is-rounded" on:click={goLogin}>Se connecter</button>
+        <button class="button is-warning is-light is-rounded" on:click={goSignin}>Creer un compte</button>
+        </div> 
+    </div>
+    <div class="hero-body">
+                <figure class="image">
+                    <img src="{logo}" />
+                </figure>
+       </div>
+    <div class="hero-foot"></div>
+</section>
+
+<style>
+    .header-image {
+    /* background-image: url("http://orig14.deviantart.net/7584/f/2015/181/2/7/flat_mountains_landscape_by_ggiuliafilippini-d8zdbco.jpg"); */
+    background-image: url("$lib/img/app/hero2.jpg");
+    
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-color: #999;
+}
+</style>
