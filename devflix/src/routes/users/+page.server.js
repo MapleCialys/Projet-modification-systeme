694f4c31a3039edc2a051dd2d8e4d2a36ac43f55
Aurore Langlet <aurore_langlet@etik.com>
2024-08-29 15:32:29 -0400

    Sécurité accès admin /users

--- devflix/src/routes/users/+page.server.js
+++ devflix/src/routes/users/+page.server.js
@@ -1,15 +1,16 @@
 import { findAll } from "$lib/db/controllers/users.controller.js";
 import { findOne } from "$lib/db/controllers/sessions.controller.js";
+import { redirect } from "@sveltejs/kit";
 
 export async function load({params, cookies}) {
     const users = await findAll();
     const session = cookies.get("session");
-    let id_user = null;
-    if(session)
+    const id_user = await findOne({uuid: session});
+    if(id_user.users.dataValues.role_id != 1)
     {
-        id_user = await findOne({uuid: session});
-        return { users:users, user: id_user.users.dataValues };
+        console.log('Utilisateur non autorisé' , id_user.users);
+        const userId = id_user.users.dataValues.id;
+        redirect(308, `/users/${userId}`);
     }
-    else
-    return { users:users }
+    return { users:users, user: id_user.users.dataValues };
 }
