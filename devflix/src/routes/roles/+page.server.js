c7cfc32d590f71da772aded3da62ead63ff12803
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-08-29 16:41:24 -0400

    jsdoc +page.server.js role

--- devflix/src/routes/roles/+page.server.js
+++ devflix/src/routes/roles/+page.server.js
@@ -1,5 +1,10 @@
 import { findAll } from "../../lib/db/controllers/roles.controller";
 
+/**
+ * Charge tous les rôles.
+ * @param {Object} params - Les paramètres de la requête.
+ * @returns {Object} - La liste des rôles.
+ */
 export async function load({params}) {
     const roles = await findAll();
     return { roles:roles };
