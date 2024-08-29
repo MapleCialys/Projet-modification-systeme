8c1fbb4531b30f7141f7404b8c09ef5c54e2240a
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-08-29 16:42:18 -0400

    jsdoc +page.server.js role/new

--- devflix/src/routes/roles/new/+page.server.js
+++ devflix/src/routes/roles/new/+page.server.js
@@ -3,6 +3,13 @@ import { newRole } from "$lib/db/controllers/roles.controller";
 
 export const actions = {
 
+/**
+ * Crée un nouveau rôle.
+ * @param {Object} cookies - Les cookies de la requête.
+ * @param {Object} request - La requête.
+ * @returns {void}
+ */
+export 
     new: async({ cookies, request })=>{
         const data = await request.formData();
 
