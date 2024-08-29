29a3b19df11b1f6d62a2c15cb8c0402e9cd6743e
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-08-29 16:35:24 -0400

    jsdoc +page.server.js logout

--- devflix/src/routes/logout/+page.server.js
+++ devflix/src/routes/logout/+page.server.js
@@ -2,6 +2,11 @@ import { fail } from '@sveltejs/kit';
 import { deleteCookie } from '../../lib/db/controllers/sessions.controller';
 import { redirect } from '@sveltejs/kit';
 
+/**
+ * Gère la déconnexion de l'utilisateur.
+ * @param {Object} cookies - Les cookies de la requête.
+ * @returns {Object} - Résultat de la déconnexion.
+ */
 export async function load({ cookies })
 {
     try {
