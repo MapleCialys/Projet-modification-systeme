d8f4f32c08527afd2f205278e8d2ebc6e78d1cef
MapleCialys <160977634+MapleCialys@users.noreply.github.com>
2024-08-29 16:43:11 -0400

    jsdoc +page.server.js role/id

--- devflix/src/routes/roles/[id]/+page.server.js
+++ devflix/src/routes/roles/[id]/+page.server.js
@@ -1,5 +1,10 @@
 import { findOne } from "$lib/db/controllers/roles.controller.js";
 
+/**
+ * Charge un rôle spécifique en fonction de l'identifiant.
+ * @param {Object} params - Les paramètres de la requête.
+ * @returns {Object} - Le rôle spécifié.
+ */
 export async function load({ params }){
     const role = await findOne({id:params.id});
     return { role:role }
